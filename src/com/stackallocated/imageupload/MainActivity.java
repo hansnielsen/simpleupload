6f0b54869b734bd0d4beef1f1ed1980280432371
Hans Nielsen <hans@stackallocated.com>
2014-09-21 16:35:18 -0700

    Some import cleanup

--- src/com/stackallocated/imageupload/MainActivity.java
+++ src/com/stackallocated/imageupload/MainActivity.java
@@ -3,10 +3,8 @@ package com.stackallocated.imageupload;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private final static String TAG = "MainActivity";
