291d6506c3f45d5b0bde0adb31aeb4e2424f6fac
Hans Nielsen <hans@stackallocated.com>
2014-09-21 15:30:16 -0700

    Add Settings activity

--- src/com/stackallocated/imageupload/MainActivity.java
+++ src/com/stackallocated/imageupload/MainActivity.java
@@ -32,6 +32,8 @@ public class MainActivity extends Activity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
             return true;
         }
         return super.onOptionsItemSelected(item);
